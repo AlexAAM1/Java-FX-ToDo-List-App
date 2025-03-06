f251be0228502e83330ad7dc1565c7f57a562945
AlexAAM1 <98554045+AlexAAM1@users.noreply.github.com>
2025-03-06 03:19:33 -0600

    Add files via upload

---
+++ JavaFX-FilteredList-Source-code/src/com/timbuchalka/todolist/DialogController.java
@@ -0,0 +1,36 @@
+package com.timbuchalka.todolist;
+
+import com.timbuchalka.todolist.datamodel.TodoData;
+import com.timbuchalka.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+/**This is an example project/tutorial I followed
+ * Created by timbuchalka on 8/05/2016.
+ */
+public class DialogController {
+
+    @FXML
+    private TextField shortDescriptionField;
+
+    @FXML
+    private TextArea detailsArea;
+
+    @FXML
+    private DatePicker deadlinePicker;
+
+    //This method creates a TodoItem object from the user's input into each member of the TodoItem class
+    public TodoItem processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription, details, deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
+}
